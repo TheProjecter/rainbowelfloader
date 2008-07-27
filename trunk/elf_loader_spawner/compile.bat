3018d198c51ba2d942b7761c918c10c8d33ea211
thecor3 <thecor3@55b1fc06-7652-0410-b9ce-2f6ddea81521>
2008-07-27 09:19:00 +0000

    -mbig-endian arg during compilation

--- trunk/elf_loader_spawner/compile.bat
+++ trunk/elf_loader_spawner/compile.bat
@@ -1,4 +1,4 @@
-mcore-elf-gcc -ISDK -nostdinc -fomit-frame-pointer -nostdlib -fno-builtin -c -o main.o main.c 
+mcore-elf-gcc -mbig-endian -ISDK -nostdinc -fomit-frame-pointer -nostdlib -fno-builtin -c -o main.o main.c 
 mcore-elf-ld -Bstatic -Tlinker.ld -oout.bin 
 
 pause
