dd917a669aaf4e06e8648c44411d9978b873a10c
thecor3 <thecor3@55b1fc06-7652-0410-b9ce-2f6ddea81521>
2008-07-28 08:16:20 +0000

    New way to manage imports, thanks to Chik

--- trunk/elf_loader_spawner/compile.bat
+++ trunk/elf_loader_spawner/compile.bat
@@ -1,4 +1,8 @@
-mcore-elf-gcc -mbig-endian -ISDK -nostdinc -fomit-frame-pointer -nostdlib -fno-builtin -c -o main.o main.c 
-mcore-elf-ld -Bstatic -Tlinker.ld -oout.bin 
+mcore-elf-gcc -mbig-endian -nostdinc -I../SDK -fomit-frame-pointer -nostdlib -fno-builtin -c -o main.o main.c
+mcore-elf-as -EB functions.asm
+mcore-elf-ld -o functions.elf -EB a.out
+mcore-elf-ld -Bstatic -Tlinker.ld -oout.bin
 
+del *.out
+del *.o
 pause
--- trunk/elfloader/compile.bat
+++ trunk/elfloader/compile.bat
@@ -1,5 +1,8 @@
-mcore-elf-gcc -mbig-endian -nostdinc -ISDK -fomit-frame-pointer -nostdlib -fno-builtin -c -o loader.o loader.c
+mcore-elf-gcc -mbig-endian -nostdinc -I../SDK -fomit-frame-pointer -nostdlib -fno-builtin -c -o loader.o loader.c
+mcore-elf-as -EB functions.asm
+mcore-elf-ld -o functions.elf -EB a.out
 mcore-elf-ld -Bstatic -Tlinker.ld -oldr.bin
 
+del *.out
 del *.o
 pause
