f42a4b5f9100adea0864824b5de81b29a4867f09
thecor3 <thecor3@55b1fc06-7652-0410-b9ce-2f6ddea81521>
2008-07-30 10:37:01 +0000

    Working L macro, thanks Chik Now multilang

--- elfloader/compile.bat
+++ elfloader/compile.bat
@@ -1,8 +1,9 @@
-mcore-elf-gcc -mbig-endian -nostdinc -I../SDK -fomit-frame-pointer -nostdlib -fno-builtin -c -o loader.o loader.c
+mcore-elf-gcc -fshort-wchar -mbig-endian -nostdinc -I../SDK -fomit-frame-pointer -nostdlib -fno-builtin -c -o loader.o loader.c
 mcore-elf-as -EB functions.asm
 mcore-elf-ld -o functions.elf -EB a.out
 mcore-elf-ld -Bstatic -Tlinker.ld -oldr.bin
 
 del *.out
+del *.elf
 del *.o
 pause
