0f21310738f2e1f77f9335d5804daba56081c829
thecor3 <thecor3@55b1fc06-7652-0410-b9ce-2f6ddea81521>
2008-08-07 19:22:35 +0000

    A

---
+++ trunk/applications/compile.bat
@@ -0,0 +1,15 @@
+REM Example compile file
+
+mcore-elf-gcc -fshort-wchar -mbig-endian -nostdinc -m340 -I..\SDK -fomit-frame-pointer -nostdlib -c -o GetMyPDS\GetMyPDS.o GetMyPDS\GetMyPDS.c
+mcore-elf-ld -dn -r -no-bss-init -TGetMyPDS\linker.ld -oGetMyPDS.elf
+mcore-elf-gcc -fshort-wchar -mbig-endian -nostdinc -m340 -I..\SDK -fomit-frame-pointer -nostdlib -fno-builtin -c -o FlexEditor\FlexEditor.o FlexEditor\FlexEditor.c
+mcore-elf-ld -dn -r -no-bss-init -TFlexEditor\linker.ld -oFlexEditor.elf
+mcore-elf-gcc -fshort-wchar -mbig-endian -nostdinc -m340 -I..\SDK -fomit-frame-pointer -nostdlib -fno-builtin -c -o Info\Info.o Info\Info.c
+mcore-elf-ld -dn -r -no-bss-init -TInfo\linker.ld -oInfo.elf
+mcore-elf-gcc -fshort-wchar -mbig-endian -nostdinc -m340 -I..\SDK -fomit-frame-pointer -nostdlib -fno-builtin -c -o MultiBkg\MultiBkg.o MultiBkg\MultiBkg.c
+mcore-elf-ld -dn -r -no-bss-init -TMultiBkg\linker.ld -oMultiBkg.elf
+mcore-elf-gcc -fshort-wchar -mbig-endian -nostdinc -m340 -I..\SDK -fomit-frame-pointer -nostdlib -fno-builtin -c -o SwitchToBoot\SwitchToBoot.o SwitchToBoot\SwitchToBoot.c
+mcore-elf-ld -dn -r -no-bss-init -TSwitchToBoot\linker.ld -oSwitchToBoot.elf
+
+del /S *.o
+pause
