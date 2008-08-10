d8d8a220ce8e77d81f3cf41579d633acec333f8f
thecor3 <thecor3@55b1fc06-7652-0410-b9ce-2f6ddea81521>
2008-08-10 18:50:56 +0000

    Clock init added Many many many thanks to yakk :)

--- CPOGBootLoader/bootloader/ll_phone.h
+++ CPOGBootLoader/bootloader/ll_phone.h
@@ -11,5 +11,9 @@
     int32 port_read_l( uint32 port );
     int16 port_read_w( uint32 port );
     int8 port_read_b( uint32 port );
+    
+    void HAPI_GPIO_config( int8, int16, int16 );
+
+    void pu_main_set_powerup_reason( int8 );
 
 #endif
