a55a16fdf9f9a17dca367a186f81f7b20c303062
thecor3 <thecor3@55b1fc06-7652-0410-b9ce-2f6ddea81521>
2008-08-12 15:43:44 +0000

    Removed

--- trunk/CPOGBootLoader/bootloader/devs.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DEVS_H_
-#define _DEVS_H_
-
-    #define WATCHDOG_BASE_ADDR 0x20E000
-    #define WATCHDOG_SERVICE   WATCHDOG_BASE_ADDR + 2
-    
-    #define CLOCK_BASE_ADDR    0x201000
-
-#endif
