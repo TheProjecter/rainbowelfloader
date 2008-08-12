136e6798824a884dc2af43d286ffda3ee5f408da
thecor3 <thecor3@55b1fc06-7652-0410-b9ce-2f6ddea81521>
2008-08-12 15:43:44 +0000

    Removed

--- CPOGBootLoader/compile.bat
+++
@@ -1,8 +0,0 @@
-mcore-elf-as -cpu=340 -EB boot.asm -o boot.o
-mcore-elf-gcc -mbig-endian -Iinclude -Ibootloader -nostdinc -nostdlib -fno-builtin -c -o main.o main.c
-mcore-elf-gcc -mbig-endian -Iinclude -Ibootloader -nostdinc -nostdlib -fno-builtin -c -o keyboard.o keyboard.c
-mcore-elf-gcc -mbig-endian -Iinclude -Ibootloader -nostdinc -nostdlib -fno-builtin -c -o ll_phone.o ll_phone.c
-mcore-elf-ld -O -nostdlib -Bstatic -Tlinker.ld -oCG17.smg
-
-del *.o
-pause
